29941a67e10a8f6488f7fe50adbd54af7d6a22af
Robin Vuyyuru <robinvuy@MASTUDENT-Setup-User-C17GMB1VQ6L4.local>
2023-03-21 15:35:01 +0000

    Test drive a single-class program, exercise

--- lib/diary_entry.rb
+++ lib/diary_entry.rb
@@ -16,13 +16,15 @@ class DiaryEntry
 
   def count_words
     # Returns the number of words in the contents as an integer
-    
+    return @contents.split(" ").length
   end
 
   def reading_time(wpm) # wpm is an integer representing the number of words the
                         # user can read per minute
     # Returns an integer representing an estimate of the reading time in minutes
     # for the contents at the given wpm.
+    fail "wpm must be above 0" unless wpm > 0
+    return (count_words / wpm.to_f).ceil
   end
 
   def reading_chunk(wpm, minutes) # `wpm` is an integer representing the number
@@ -34,9 +36,19 @@ class DiaryEntry
     # If called again, `reading_chunk` should return the next chunk, skipping
     # what has already been read, until the contents is fully read.
     # The next call after that it should restart from the beginning.
+    #if @word_count <= (@wpm/minutes)
+      #return @contents
+    #elsif @word_count > (@wpm/minutes)
+      #@wordindex = @word_count/(@wpm/minutes)
+     # return "#{@contents[0]}..#{@contents[@wordindex]}"
+    #end
+  
   end
 end
 
-diary = DiaryEntry.new("cool", "hi")
-puts diary.title
-puts diary.contents
+#diary = DiaryEntry.new("cool", "hi " * 400)
+#puts diary.title
+#puts diary.contents
+#puts diary.count_words
+#puts diary.reading_time(200)
+#puts diary.reading_chunk(200, 1)
--- spec/diary_entry_spec.rb
+++ spec/diary_entry_spec.rb
@@ -1,12 +1,47 @@
 require "diary_entry" 
 
 RSpec.describe DiaryEntry do
-  it "recieves a title and contents, and provides wpm, reading time and reading chunk" do
+  it "returns title and contents strings" do
     diary_entry = DiaryEntry.new("Coding for dummies", "Step 1, get a computer")
-    diary_entry.title
-    diary_entry.contents
-    diary_entry.count_words(contents)
-    diary_entry.reading_time(x)
-    expect(diary_entry.reading_chunk).to eq "Step 1, get a computer"
+    expect(diary_entry.title).to eq "Coding for dummies"
+    expect(diary_entry.contents).to eq "Step 1, get a computer"
   end
+
+  describe "#count words" do
+    it "returns number of words in contents as integer" do
+      diary_entry = DiaryEntry.new("Coding for dummies", "Step 1, get a computer")
+      expect(diary_entry.count_words).to eq 5
+  end
+
+    it "returns 0 for an empty string" do
+      diary_entry = DiaryEntry.new("Coding for dummies", "")
+      expect(diary_entry.count_words).to eq 0
+    end
+  end
+
+  describe "#reading_time" do
+    context "given a wpm of 200" do
+      it "returns number of words in contents as integer" do
+        diary_entry = DiaryEntry.new("Coding for dummies", "one " * 550 )
+        expect(diary_entry.reading_time(200)).to eq 3
+      end
+    end
+    
+    context "given a wpm of 0" do
+      it "fails" do
+        diary_entry = DiaryEntry.new("Coding for dummies", "one " * 550 )
+        expect { diary_entry.reading_time(0) }.to raise_error "wpm must be above 0"   
+      end
+    end
+  end
+
+  describe "#reading_chunk" do
+    context "given 1 minute" do
+      it "return chunk of contents that can be read within minutes according to wpm" do
+        diary_entry = DiaryEntry.new("Coding for dummies", "step one, get a computer")
+        expect(diary_entry.reading_chunk(2, 1)).to eq "step one"
+      end
+    end
+  end
+  
 end
