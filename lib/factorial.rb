66d1f64109e1f166d930d3389229dc5e139ed506
Robin Vuyyuru <robinvuy@MASTUDENT-Setup-User-C17GMB1VQ6L4.local>
2023-03-22 18:58:04 +0000

    Intermezzo, debugging 2

---
+++ lib/factorial.rb
@@ -0,0 +1,13 @@
+# File: factorial.rb
+
+def factorial(n)
+    product = 1
+  while n > 0
+    binding.irb # Mystery new line!
+    product *= n
+    n -= 1
+  end
+    product
+end
+
+p factorial(5)
