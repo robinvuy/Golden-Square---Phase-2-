335b8efdc83c9fedaae619acd32884bd8e8e0504
Robin Vuyyuru <robinvuy@MASTUDENT-Setup-User-C17GMB1VQ6L4.local>
2023-03-22 11:22:11 +0000

    test drive a single class program, exercise

--- spec/diary_entry_spec.rb
+++ spec/diary_entry_spec.rb
@@ -36,12 +36,22 @@ RSpec.describe DiaryEntry do
   end
 
   describe "#reading_chunk" do
-    context "given 1 minute" do
+    context "with a contents unreadable within the time" do
       it "return chunk of contents that can be read within minutes according to wpm" do
-        diary_entry = DiaryEntry.new("Coding for dummies", "step one, get a computer")
-        expect(diary_entry.reading_chunk(2, 1)).to eq "step one"
+        diary_entry = DiaryEntry.new("Coding for dummies", "one two three")
+        chunk = diary_entry.reading_chunk(2, 1)
+        expect(chunk).to eq "one two"
+      end
+
+      it "returns the next chunk, next time we are asked" do
+        diary_entry = DiaryEntry.new("Coding for dummies", "one two three")
+        diary_entry.reading_chunk(2, 1)
+        chunk = diary_entry.reading_chunk(2, 1)
+        expect(chunk).to eq "three"
       end
     end
+  
+  
   end
   
 end
