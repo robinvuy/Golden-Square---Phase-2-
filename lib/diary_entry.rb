41a93f2f2bbefb68c037b6b09db5bcf82a169873
Robin Vuyyuru <robinvuy@MASTUDENT-Setup-User-C17GMB1VQ6L4.local>
2023-03-22 11:21:22 +0000

    Test drive a single class program, exercise

--- lib/diary_entry.rb
+++ lib/diary_entry.rb
@@ -2,6 +2,7 @@ class DiaryEntry
   def initialize(title, contents) # title, contents are strings
     @title = title
     @contents = contents
+    @furthest_word_read = 0
   end
 
   def title
@@ -16,14 +17,14 @@ class DiaryEntry
 
   def count_words
     # Returns the number of words in the contents as an integer
-    return @contents.split(" ").length
+    return words.length
   end
 
   def reading_time(wpm) # wpm is an integer representing the number of words the
                         # user can read per minute
     # Returns an integer representing an estimate of the reading time in minutes
     # for the contents at the given wpm.
-    fail "wpm must be above 0" unless wpm > 0
+    fail "wpm must be above 0" unless wpm.positive?
     return (count_words / wpm.to_f).ceil
   end
 
@@ -42,13 +43,27 @@ class DiaryEntry
       #@wordindex = @word_count/(@wpm/minutes)
      # return "#{@contents[0]}..#{@contents[@wordindex]}"
     #end
-  
+    no_of_words_we_can_read = wpm * minutes
+    start_from = @furthest_word_read
+    end_at = @furthest_word_read + no_of_words_we_can_read
+    word_list = words[start_from, end_at]
+    @furthest_word_read = end_at
+    return word_list.join(" ")
   end
 end
 
-#diary = DiaryEntry.new("cool", "hi " * 400)
-#puts diary.title
-#puts diary.contents
-#puts diary.count_words
-#puts diary.reading_time(200)
-#puts diary.reading_chunk(200, 1)
+
+  private
+
+  def words
+    return @contents.split(" ")
+  end
+
+ 
+
+diary = DiaryEntry.new("cool", "hi im a cool kid")
+puts diary.title
+puts diary.contents
+puts diary.count_words
+puts diary.reading_time(2)
+puts diary.reading_chunk(2, 1)
